00b4d4c156a00019ccd53ada432d2262b72fc34d
Rojda Polat <rojdapolat@outlook.de>
2025-02-24 17:46:58 +0100

    day1

--- src/question.js
+++ src/question.js
@@ -2,6 +2,36 @@ class Question {
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
 
     // 2. shuffleChoices()
+    shuffleChoices() {
+        this.choices.sort(() => Math.random() - 0.5);
+        return this.choices;
+    }
 }
+
+// shuffleChoices(){   
+//         for (let i = this.choices.length - 1; i > 0; i--){
+//             const j = Math.floor(Math.random() * (i + 1));
+//             [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+//         }
+//     }
+
+
+  
+    // shuffleChoices() {
+    //   //correct way to shuffle
+    //   for (let i = 0; i < this.choices.length; i++) {
+    //     let randomIndex = Math.floor(Math.random() * this.choices.length);
+    //     let choiceToMove = this.choices[i];
+    //     this.choices.splice(i, 1);
+    //     this.choices.splice(randomIndex, 0, choiceToMove);
+    //   }
+    // }
+  
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,46 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
-
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
     // 2. getQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     
     // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex ++;
+    }
 
     // 4. shuffleQuestions()
+    shuffleQuestions() {
+        this.questions.sort(() => Math.random() - 0.5);
+        return this.questions;
+    }
 
     // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        const currentQuestion = this.questions[this.currentQuestionIndex];
+        if (answer === currentQuestion.answer) {
+            this.correctAnswers++;
+        }
+    }
 
     // 6. hasEnded()
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        }
+        else if (this.currentQuestionIndex === this.questions.length) {
+            return true;
+        }
+    }
 }
+
+
